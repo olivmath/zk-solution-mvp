dfc6033d297d4026dcb09b71f3267732e276b843
Lucas Oliveira <olivmath@protonmail.com>
2025-05-20 01:04:41 -0300

    add pjg

---
+++ pkg/ultraplonk_no_std.d.ts
@@ -0,0 +1,4 @@
+/* tslint:disable */
+/* eslint-disable */
+export function convertProof(proof_data: Uint8Array, num_inputs: number): any;
+export function convertVerificationKey(vk_data: Uint8Array): any;
